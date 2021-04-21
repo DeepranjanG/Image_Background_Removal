9d358b7da128ab850b926cf52d1acf48d91bc88a
DeepranjanG <kdeepu070@gmail.com>
2021-04-22 01:29:51 +0530

    Final Commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, Response, request, session, redirect, url_for, send_from_directory, flash
+from werkzeug.utils import secure_filename
+import inference
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        # create a secure filename
+        filename = secure_filename(f.filename)
+        print("**************************")
+        print(filename)
+        # save file to /static/uploads
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        print(filepath)
+        f.save(filepath)
+        inference.predict(filepath)
+        return render_template("uploaded.html", display_detection=filename, fname=filename)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+	#print('display_image filename: ' + filename)
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
+if __name__ == '__main__':
+    app.run(port=7000, debug=True)
